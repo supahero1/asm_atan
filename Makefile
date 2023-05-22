2fb870acab6946850d8abd0fe62aa79dd784bfee
supahero1 <a@b.c>
2023-05-22 13:10:01 +0200

    opt

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
+.PHONY: all
+all: build
+
+atan.o: atan.s
+	nasm -f elf64 -Wall atan.s
+
 .PHONY: build
-build: atan.s main.c
-	nasm -f elf64 atan.s
+build: atan.o main.c
 	gcc main.c -O3 -c
 	gcc atan.o main.o -o main -lm -lshnet
 	./main
 
 .PHONY: test
-test: atan.s test.c
-	nasm -f elf64 atan.s
+test: atan.o test.c
 	gcc test.c -O0 -g3 -ggdb -c
 	gcc atan.o test.o -o test -O0 -g3 -ggdb -lm
 	./test
--- atan.s
+++ atan.s
@@ -1,14 +1,16 @@
 section .data
 align 16
-c1:	dq	0x3F5A0F91, 0, 0, 0	; 0.8518
+c1:		dd	0x3F5A0F91	; 0.8518
 align 16
-c2:	dq	0x40700000, 0, 0, 0	; 3.75
+c2:		dd	0x40700000	; 3.75
 align 16
-c3:	dq	0x3E7CD6EA, 0, 0, 0	; 1 / 4.05
+c3:		dd	0x3E7CD6EA	; 1 / 4.05
 align 16
-c4_pi:	dq	0x3FC90FDB, 0, 0, 0	; pi / 2
+c4_pi:		dd	0x3FC90FDB	; pi / 2
 align 16
-mask:	dq	0x00000000000000000000000080000000
+mask_sgn:	dd	0x80000000
+align 16
+mask_abs:	dd	0x7FFFFFFF
 
 section .text
 global empty_function
@@ -18,29 +20,28 @@ empty_function:
 align 4096
 global asm_atan
 asm_atan:
-	movaps	xmm1, xmm0
-	psllq	xmm1, 1
-	psrlq	xmm1, 1		; fabs(x)
-	movaps	xmm2, [rel c1]
-	comiss	xmm1, xmm2	; fabs(x) < c1
-	jb	less		; less likely
+	movss	xmm1, xmm0
+	andps	xmm1, [rel mask_abs]	; fabs(x)
+	movss	xmm2, dword[rel c1]
+	comiss	xmm1, xmm2		; fabs(x) < c1
+	jb	less			; less likely
 
-	movaps	xmm2, [rel c2]
-	mulss	xmm2, xmm0	; x * c2
-	rcpss	xmm2, xmm2	; 1 / (x * c2)
-	addss	xmm2, xmm0	; x + 1 / (x * c2)
-	rcpss	xmm2, xmm2	; 1 / (x + 1 / (x * c2))
+	movss	xmm2, [rel c2]
+	mulss	xmm2, xmm0		; x * c2
+	rcpss	xmm2, xmm2		; 1 / (x * c2)
+	addss	xmm2, xmm0		; x + 1 / (x * c2)
+	rcpss	xmm2, xmm2		; 1 / (x + 1 / (x * c2))
 
-	movaps	xmm1, [rel c4_pi]
-	andps	xmm0, [rel mask]; upper bit either 0 or 1
-	orps	xmm0, xmm1	; PI/2 or -PI/2
+	movss	xmm1, dword[rel c4_pi]
+	andps	xmm0, [rel mask_sgn]	; upper bit either 0 or 1
+	orps	xmm0, xmm1		; PI/2 or -PI/2
 
 	subss	xmm0, xmm2
 	ret
 less:
 	mulss	xmm1, xmm1
-	mulss	xmm1, xmm0	; fabs(x) * fabs(x) * x == x * x * x
-	movaps	xmm2, [rel c3]
-	mulss	xmm1, xmm2	; (x * x * x) * c3
-	subss	xmm0, xmm1	; x - (x * x * x)  * c3
+	mulss	xmm1, xmm0		; fabs(x) * fabs(x) * x == x * x * x
+	movss	xmm2, dword[rel c3]
+	mulss	xmm1, xmm2		; (x * x * x) * c3
+	subss	xmm0, xmm1		; x - (x * x * x)  * c3
 	ret
